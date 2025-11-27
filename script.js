26fb7395b294c9e1fe723ab12860b4286fccf018
lamia bn <lamiabane65@gmail.com>
2025-11-27 08:58:34 +0100

    update for this web site and fix some issues

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const openBtn = document.getElementById("button_add");
+const buttonExperience = document.querySelector("#addExperience");
 const modal = document.getElementById("modale");
 const remove= document.getElementById("remove");
+// button de profil X
+const displayProfil = document.querySelector(".displayProfil")
+const removebutton = document.getElementById("removebutton")
+// form inputs
+const inputName = document.querySelector("#Nom");
+const inputRole = document.querySelector("#role");
+const inputImage = document.querySelector("#photo");
+const inputEmail = document.querySelector("#Email");
+const inputTel = document.querySelector("#Telephone");
+// input experience
+const inputEntreprise = document.querySelector("#entreprise");
+const inputPoste = document.querySelector("#poste");
+const inputDateDebut = document.querySelector("#debut");
+const inputDateFin = document.querySelector("#fin");
+//buuton X
+removebutton.addEventListener("click",()=>{
+    displayProfil.classList.add("hidden");
+})
+
 const workers=[];
+let experiences=[];
+
 const list =document.getElementById("workers_list");
 affichageEmploye(workers)
 // Ouvrir la modale
@@ -10,27 +32,53 @@ openBtn.addEventListener("click", () => {
     modal.classList.add("open");
 });
 
+// Ajout d'experience
+buttonExperience.addEventListener("click", () => {
+    document.querySelector(".experiences").style.display="flex"
+})
+
+document.querySelector("#addExperience").addEventListener("click", () => {
+    const experience = {
+        employerName: inputEntreprise.value,
+        jobPosition: inputPoste.value,
+        dateOfStart: inputDateDebut.value,
+        dateOfFinish: inputDateFin.value
+    }
+
+    inputEntreprise.value = '';
+    inputPoste.value = '';
+    inputDateDebut.value = '';
+    inputDateFin.value = '';
+
+    experiences.push(experience);
+})
+
 // Bouton Submit dans la modale
 const submitBtn = modal.querySelector(".Submit");
 submitBtn.addEventListener("click", () => {
-    const nom = document.getElementById("Nom").value;
-    const role = document.getElementById("role").value;
-    const photo = document.getElementById("photo").value;
-    const email = document.getElementById("Email").value;
-    const tel = document.getElementById("Telephone").value;
-
-    if(!nom || !role || !photo || !email || !tel){
-        alert("erreur")
-        return; 
-    }
+   
    workers.push({
-    name:nom,
-    role:role,
-    photo:photo,
-    email:email,
-    tel:tel
+    name: inputName.value,
+    role:inputRole.value,
+    photo:inputImage.value,
+    email:inputEmail.value,
+    tel:inputTel.value,
+    exps: experiences
    })
    
+   inputEntreprise.value = '';
+   inputPoste.value = '';
+   inputDateDebut.value = '';
+   inputDateFin.value = '';
+   inputName.value = "";
+   inputRole.value = "";
+   inputImage.value = "";
+   inputEmail.value = "";
+   inputTel.value = "";
+   experiences = [];
+
+   console.log(workers)
+
     modal.classList.add("hidden");
     modal.classList.remove("open");
     list.innerHTML = "";
@@ -43,7 +91,7 @@ modal.addEventListener("click",(e)=>{
         modal.classList.add("hidden");
     }
 })
- 
+
 function affichageEmploye(w){
     w.forEach(ele => {
         const profil= document.createElement("div");
